2692ecd7cd6eabe7418e69e851bf536ceda6b6ca
Alan M.E <alanmarquez@outlook.com>
2018-12-10 22:03:48 -0500

    created componet App
    App: Manage views

---
+++ Fumoffu-chat-front/src/app/index.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+import './app.scss';
+
+export default class App extends Component {
+
+    render() {
+        return (
+            <div className="chat">
+                <div className="navbar"> 
+                    <section className="user-status">
+                        <figure className="avatar avatar-lg" data-initial="YZ">
+                            <i className="avatar-presence online"></i>
+                        </figure>
+                    </section> 
+                    <section className="menu">
+                        <div className="menu-item"> <i className="large material-icons"> home </i> </div>
+                        <div className="menu-item select"> <i className="large material-icons"> chat </i> </div>
+                        <div className="menu-item"> <i className="large material-icons"> group </i> </div>
+                        <div className="menu-item"> <i className="large material-icons"> settings </i> </div>
+                    </section> 
+                    <section className="options">
+                        <div className="options-item"> <i class="large material-icons"> exit_to_app </i> </div>
+                    </section> 
+                </div>
+            </div>
+        )
+    }
+
+}
