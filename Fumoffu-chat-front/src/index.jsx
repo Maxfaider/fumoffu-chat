5fa80792a7a52888d5803d69239d95adee6a9d73
Alan M.E <alanmarquez@outlook.com>
2018-12-10 22:08:05 -0500

    updated entry page

---
+++ Fumoffu-chat-front/src/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+import AppContext from './util/appcontext';
+
+import App from './app';
+
+import './style.scss';
+
+AppContext.addAttr("name-app", "Fumoffu-Chat");
+AppContext.addAttr("version", "v1.0.0");
+AppContext.addAttr("maintainer", "Alan M.E <alanmarquez@outlook.com>");
+
+render(<App />, document.getElementById('root'));
