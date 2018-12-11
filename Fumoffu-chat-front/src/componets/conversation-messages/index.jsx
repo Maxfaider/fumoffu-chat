7cb33f48a746e34e129bfd4992f87b488b1f64c4
Alan M.E <alanmarquez@outlook.com>
2018-12-11 13:21:50 -0500

    created ConversationMessages component

---
+++ Fumoffu-chat-front/src/componets/conversation-messages/index.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+import './conversation-messages.scss';
+
+export default class ConversationMessages extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <section className="conversation-messages">
+
+            </section>
+        )
+    }
+
+
+}
