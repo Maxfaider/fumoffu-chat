e0cabbdeaf2947814561679a046a9ec8c808d36a
Alan M.E <alanmarquez@outlook.com>
2018-12-27 17:52:18 -0500

    removed method renderView

--- Fumoffu-chat-front/src/views/viewbuilder.jsx
+++ Fumoffu-chat-front/src/views/viewbuilder.jsx
@@ -3,14 +3,6 @@ import React from 'react';
 import ChatZone from './chat-zone';
 
 export default {
-    renderView(name) {
-        switch(name) {
-            case 'HomeZone': return this.createHomeZone();
-            case 'ChatZone': return this.createChatZone();
-            case 'GroupsZone': return this.createGroupZone();
-            case 'SettingsZone': return this.createSettingsZone();
-        }
-    },
     renderHomeZone() {
         return <div> Home </div>
     },
