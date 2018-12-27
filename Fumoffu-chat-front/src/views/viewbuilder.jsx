c64050026414439b3786cba95ae62085e5c2a905
Alan M.E <alanmarquez@outlook.com>
2018-12-27 17:41:04 -0500

    updated ViewBuilder: chat-zone

--- Fumoffu-chat-front/src/views/viewbuilder.jsx
+++ Fumoffu-chat-front/src/views/viewbuilder.jsx
@@ -3,7 +3,27 @@ import React from 'react';
 import ChatZone from './chat-zone';
 
 export default {
-    createChatZone() {
+    renderView(name) {
+        switch(name) {
+            case 'HomeZone': return this.createHomeZone();
+            case 'ChatZone': return this.createChatZone();
+            case 'GroupsZone': return this.createGroupZone();
+            case 'SettingsZone': return this.createSettingsZone();
+        }
+    },
+    renderHomeZone() {
+        return <div> Home </div>
+    },
+    renderChatZone() {
         return <ChatZone />
+    },
+    renderGroupsZone() {
+        return <div> Groups </div>
+    },
+    renderSettingsZone() {
+        return <div> Settings </div>
+    },
+    renderLogin() {
+        return <div> Login </div>
     }
 }
