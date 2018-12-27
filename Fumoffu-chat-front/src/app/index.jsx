53ec0ba81e567c616ec3eb7c343c8595f947074c
Alan M.E <alanmarquez@outlook.com>
2018-12-27 17:42:58 -0500

    added interactive menu

--- Fumoffu-chat-front/src/app/index.jsx
+++ Fumoffu-chat-front/src/app/index.jsx
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
+import { Route, BrowserRouter, Switch, NavLink } from 'react-router-dom'
 
 import Viewbuilder from '../views/viewbuilder';
 
 import './app.scss';
 
 export default class App extends Component {
-
+    constructor(props) {
+        super(props);
+        this.status = {
+            currentView: 'ChatZone'
+        }
+        
+        this.onShowView.bind(this);
+    }
+    
+    onShowView(evt) {
+        console.log(evt.target.name);
+        //if(nameView !== this.status.currentView)
+        //    this.status.currentView = nameView;
+    }
+    
     render() {
         return (
+            <BrowserRouter>
             <div className="chat">
                 <div className="navbar"> 
                     <section className="user-status">
@@ -16,18 +32,24 @@ export default class App extends Component {
                         </figure>
                     </section> 
                     <section className="menu">
-                        <div className="menu-item"> <i className="large material-icons"> home </i> </div>
-                        <div className="menu-item select"> <i className="large material-icons"> chat </i> </div>
-                        <div className="menu-item"> <i className="large material-icons"> group </i> </div>
-                        <div className="menu-item"> <i className="large material-icons"> settings </i> </div>
+                        <NavLink className="menu-item" exact to="/" activeClassName="select"> <i className="large material-icons"> home </i> </NavLink> 
+                        <NavLink className="menu-item" to="/chat" activeClassName="select"> <i className="large material-icons"> chat </i> </NavLink>
+                        <NavLink className="menu-item" to="/group" activeClassName="select"> <i className="large material-icons"> group </i> </NavLink> 
+                        <NavLink className="menu-item" to="/settings" activeClassName="select"> <i className="large material-icons"> settings </i> </NavLink> 
                     </section> 
                     <section className="options">
-                        <div className="options-item"> <i className="large material-icons"> exit_to_app </i> </div>
+                        <button className="options-item"> <i className="large material-icons"> exit_to_app </i> </button>
                     </section> 
                 </div>
-                { Viewbuilder.createChatZone() }
+                <Switch>
+                    <Route path="/" exact component={Viewbuilder.renderHomeZone} />
+                    <Route path="/chat" component={Viewbuilder.renderChatZone} />
+                    <Route path="/group" component={Viewbuilder.renderGroupsZone} />
+                    <Route path="/settings" component={Viewbuilder.renderSettingsZone} />
+                    <Route component={Viewbuilder.renderHomeZone}/>
+                </Switch>
             </div>
+            </BrowserRouter>
         )
     }
-
 }
