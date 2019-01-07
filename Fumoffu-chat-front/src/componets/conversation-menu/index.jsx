3e227f41716a8a2e2e15326069c042bd7bd4a663
Alan M.E <alanmarquez@outlook.com>
2019-01-07 16:13:03 -0500

    refined conversation-menu components

--- Fumoffu-chat-front/src/componets/conversation-menu/index.jsx
+++ Fumoffu-chat-front/src/componets/conversation-menu/index.jsx
@@ -3,21 +3,41 @@ import React, { Component } from 'react';
 import './conversation-menu.scss';
     
 export default class ConversationMenu extends Component {
+    constructor(props) {
+        super(props);
+
+        this.onKeyboardVoiceHandle = this.onKeyboardVoiceHandle.bind(this);
+        this.onVideoCamHandle = this.onVideoCamHandle.bind(this);
+        this.onMoreOptionsHandle = this.onMoreOptionsHandle.bind(this);
+    }
+
+    onKeyboardVoiceHandle() {
+        this.props.keyboardVoiceHandle(this.props.friend);
+    }
+
+    onVideoCamHandle() {
+        this.props.videoCamHandle(this.props.friend);
+    }
+
+    onMoreOptionsHandle() {
+        this.props.moreOptionsHandle(this.props.friend);
+    }
+
     render() {
         return (
             <section className="conversation-menu">
                 <div className="user-indentifier">
-                    <div className="title"> Alan M.E </div>
-                    <div className="subtitle"> last seen 2 hours ago </div>
+                    <div className="title"> { this.props.friend.username } </div>
+                    <div className="subtitle"> #active time </div>
                 </div>
-                <button className="btn btn-conversation-menu">
-                    <i className="material-icons btn-gray font-large"> keyboard_voice </i>
+                <button onClick={this.onKeyboardVoiceHandle} className="btn btn-conversation-menu">
+                    <i className="material-icons font-large"> keyboard_voice </i>
                 </button>
-                <button className="btn btn-conversation-menu">
-                    <i className="material-icons btn-gray font-large"> videocam </i>
+                <button onClick={this.onVideoCamHandle} className="btn btn-conversation-menu">
+                    <i className="material-icons font-large"> videocam </i>
                 </button>
-                <button className="btn btn-conversation-menu">
-                    <i className="material-icons btn-gray font-large"> more_vert </i>
+                <button onClick={this.onMoreOptionsHandle} className="btn btn-conversation-menu">
+                    <i className="material-icons font-large"> more_vert </i>
                 </button>
             </section>
         )
