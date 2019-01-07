95e00f3b08eb75a87d270954d2664cba5bdf78e3
Alan M.E <alanmarquez@outlook.com>
2019-01-07 16:40:23 -0500

    refined search components

--- Fumoffu-chat-front/src/componets/search/index.jsx
+++ Fumoffu-chat-front/src/componets/search/index.jsx
@@ -3,10 +3,20 @@ import React, { Component } from 'react';
 import './search.scss';
 
 export default class Search extends Component {
+    constructor(props) {
+        super(props);
+
+        this.onFilterFriendHandle = this.onFilterFriendHandle.bind(this);
+    }
+
+    onFilterFriendHandle(evt) {
+        this.props.filterFriendHandle(evt.target.value);
+    }
+
     render() {
         return (
             <section className="search">
-                <input className="search-text" type="text" placeholder="Search" />
+                <input onChange={this.onFilterFriendHandle} className="search-text" type="text" placeholder="Search" />
             </section>
         )
     }
