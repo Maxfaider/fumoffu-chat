0e3dccc4ec9fcde4f02c352837c6196ae4e30818
Alan M.E <alanmarquez@outlook.com>
2019-01-07 14:13:27 -0500

    refined props Card components

--- Fumoffu-chat-front/src/componets/card/index.jsx
+++ Fumoffu-chat-front/src/componets/card/index.jsx
@@ -2,21 +2,45 @@ import React, { Component } from 'react';
 
 import './card.scss';
 
-export default class Card extends Component {
+class Card extends Component {
     constructor(props) {
         super(props);
+
+        this.sendEventClick = this.sendEventClick.bind(this);
+    }
+
+    buildAvatar() {
+        var nameShort = this.props.friend.username
+            .substr(0, 3)
+            .toUpperCase();
+
+        if (this.props.friend.profileUrl)
+            return (
+                <figure className="avatar avatar-lg">
+                    <img src={this.props.friend.profileUrl} alt={nameShort}></img>
+                    <i className="avatar-presence online"></i>
+                </figure>
+            )
+        else 
+            return (
+                <figure className="avatar avatar-lg" data-initial={nameShort}>
+                    <i className="avatar-presence online"></i>
+                </figure>
+            )
+    }
+
+    sendEventClick() {
+        this.props.clickEventHandle(this.props.friend);
     }
     
     render() {
         return (
             <div className="card">
                 <div className="card-header">
-                    <figure className="avatar avatar-lg" data-initial="YZ">
-                        <i className="avatar-presence online"></i>
-                    </figure>
-                    <div className="card-user">
-                        <div className="card-title"> { this.props.friend.title } </div>
-                        <div className="card-subtitle">{ this.props.friend.subtitle }</div>
+                    { this.buildAvatar() }
+                    <div onClick={ this.sendEventClick } className="card-user">
+                        <div className="card-title"> { this.props.friend.username } </div>
+                        <div className="card-subtitle">{ this.props.friend.nickname }</div>
                     </div> 
                     <div className="card-date">
                         { this.props.friend.date }
@@ -26,7 +50,8 @@ export default class Card extends Component {
                     { this.props.friend.lastMessage } 
                 </div>
             </div>
-            )
-        }
-        
+        )
     }
+}
+
+export default Card;
