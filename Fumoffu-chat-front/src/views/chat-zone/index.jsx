634d18468abeb5acfc33562788e8dc261ce4255a
Alan M.E <alanmarquez@outlook.com>
2019-01-07 17:07:58 -0500

    events declared in chat-zone view

--- Fumoffu-chat-front/src/views/chat-zone/index.jsx
+++ Fumoffu-chat-front/src/views/chat-zone/index.jsx
@@ -16,24 +16,105 @@ import './chat-zone.scss';
     - Other: Call, VideoCam, Block, Delete Messages, show Profile Friend
 */
 export default class ChatZone extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            friendCurrent: {
+                id: "asfrt345",
+                profileUrl: "https://avatars3.githubusercontent.com/u/7024527?s=400&v=4",
+                username: "Alan M.E",
+                nickname: "Maxfaider",
+                date: "5min ago",
+                lastMessage: "Pellentesque dictum suscipit congue."
+            },
+            friends: [
+                {
+                    id: "asfrt345",
+                    profileUrl: "https://avatars3.githubusercontent.com/u/7024527?s=400&v=4",
+                    username: "Alan M.E",
+                    nickname: "Maxfaider",
+                    date: "5min ago",
+                    lastMessage: "Pellentesque dictum suscipit congue."
+                },
+                {
+                    id: "asft213",
+                    profileUrl: "",
+                    username: "James Gordon",
+                    nickname: "Dectective",
+                    date: "2h ago",
+                    lastMessage: "Pellentesque dictum suscipit congue."
+                }
+            ]
+        }
+
+        this.onSelectFriend = this.onSelectFriend.bind(this);
+        this.onSendVoiceMessage = this.onSendVoiceMessage.bind(this);
+        this.onVideoCall = this.onVideoCall.bind(this);
+        this.onMoreOptions = this.onMoreOptions.bind(this);
+        this.onAddFriend = this.onAddFriend.bind(this);
+        this.onAttachmentFile = this.onAttachmentFile.bind(this);
+        this.onSendMessage = this.onSendMessage.bind(this);
+        this.onChangeTxtMessage = this.onChangeTxtMessage.bind(this);
+        this.onFilterFriend = this.onFilterFriend.bind(this);
+    }
+
+    onSelectFriend(friendCurrent) {
+        this.setState( {friendCurrent: friendCurrent} );
+    }
+
+    onSendVoiceMessage(friend) {
+
+    }
+
+    onVideoCall(friend) {
+
+    }
+
+    onMoreOptions(friend) {
+
+    }
+
+    onAddFriend(evt) {
+        //this.setState( {friends: this.state.friends.concat([friendCurrent])} );
+    }
+
+    onAttachmentFile(evt) {
+
+    }
+
+    onSendMessage(evt) {
+
+    }
+
+    onChangeTxtMessage(evt) {
+
+    }
+
+    onFilterFriend(messageCurrent) {
+        
+    }
+
     render() {
         return (
             <section className="chat-zone">
                 <section className="friends-zone"> 
-                    <Search />
-                    <FriendList />
+                    <Search filterFriendHandle={this.onFilterFriend}/>
+                    <FriendList friends={this.state.friends} selectFriendEventHandle={this.onSelectFriend}/>
                     <section className="add-friends">
-                        <button className="btn-round-add"><i className="material-icons"> add </i></button>
+                        <button onClick={this.onAddFriend} className="btn-round-add"><i className="material-icons"> add </i></button>
                     </section>
                 </section>
                 <section className="conversation-zone"> 
-                    <ConversationMenu />
+                    <ConversationMenu friend={this.state.friendCurrent} 
+                        keyboardVoiceHandle={this.onSendVoiceMessage}
+                        videoCamHandle={this.onVideoCall} 
+                        moreOptionsHandle={this.onMoreOptions} />
                     <ConversationMessages />
-                    <section className="conversation-sender-zone">
-                        <button className="btn-attachment"><i className="material-icons"> attachment </i></button>
-                        <input className="text-sender" type="text" placeholder="Type your message here" />
-                        <button className="btn-send"><i className="material-icons"> send </i></button>
-                    </section>
+                    <form className="conversation-sender-zone" onSubmit={this.onSendMessage}>
+                        <button onClick={this.onAttachmentFile} className="btn-attachment"><i className="material-icons"> attachment </i></button>
+                        <input onChange={this.onChangeTxtMessage} className="text-sender" type="text" name="txtMessage" placeholder="Type your message here" />
+                        <button type="submit" className="btn-send"><i className="material-icons"> send </i></button>
+                    </form>
                 </section>
             </section>
         )
