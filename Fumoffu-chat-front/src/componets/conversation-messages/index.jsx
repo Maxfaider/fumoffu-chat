8249b0aaca9ab7f2ae54886312a1756c1587f6dc
Alan M.E <alanmarquez@outlook.com>
2019-01-08 12:39:27 -0500

    refined conversation-messages component

--- Fumoffu-chat-front/src/componets/conversation-messages/index.jsx
+++ Fumoffu-chat-front/src/componets/conversation-messages/index.jsx
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 
+import MessageFriend from '../message-friend';
+import MessageOwn from '../message-own';
+
 import './conversation-messages.scss';
 
 export default class ConversationMessages extends Component {
@@ -7,13 +10,75 @@ export default class ConversationMessages extends Component {
         super(props);
     }
 
+    buildMessageFriend(id, body) {
+        return <MessageFriend 
+            key={ id }
+            text={ body } />
+    }
+
+    buildMessageFriendWithAvatar(id, body) {
+        return <MessageFriend 
+            key={ id }
+            text={ body } 
+            username={ this.props.friend.username } 
+            profileUrl={ this.props.friend.profileUrl } />
+    }
+
+    buildMessageFriendWithDate(id, body, date) {
+        return <MessageFriend 
+            key={ id }
+            text={ body } 
+            date={ date } />
+    }
+
+    buildMessageFriendWithAvatarAndDate(id, body, date) {
+        return <MessageFriend 
+            key={ id }
+            text={ body } 
+            username={ this.props.friend.username } 
+            profileUrl={ this.props.friend.profileUrl } 
+            date={ date } />
+    }
+
+    buildMessageOwn(id, body) {
+        return <MessageOwn 
+            key={ id }
+            text={ body }/> 
+    }
+
+    buildMessageOwnWithDate(id, body, date) {
+        return <MessageOwn 
+            key={ id }
+            text={ body }  
+            date={ date } />
+    }
+
     render() {
         return (
             <section className="conversation-messages">
-
+                { this.props.messages.map( (message, index, arrMessages) => {
+                    if (message.owner === 'own') {
+                        if(arrMessages[ index + 1] && arrMessages[ index + 1 ].owner === 'own') 
+                            return this.buildMessageOwn(message.id, message.body);
+                        else
+                            return this.buildMessageOwnWithDate(message.id, message.body, message.date);
+                    } else {
+                        if(arrMessages[ index - 1] && arrMessages[ index - 1 ].owner === 'own') {
+                            if(arrMessages[ index + 1] && arrMessages[ index + 1 ].owner === 'his') 
+                                return this.buildMessageFriendWithAvatar(message.id, message.body);
+                            else
+                                return this.buildMessageFriendWithAvatarAndDate(message.id, message.body, message.date);
+                        } else {
+                            if(message.owner == 'his') {
+                                if(arrMessages[ index + 1] && arrMessages[ index + 1 ].owner === 'his') 
+                                    return this.buildMessageFriend(message.id, message.body);
+                                else
+                                    return this.buildMessageFriendWithDate(message.id, message.body, message.date);
+                            }
+                        }
+                    }    
+                })}
             </section>
         )
     }
-
-
 }
