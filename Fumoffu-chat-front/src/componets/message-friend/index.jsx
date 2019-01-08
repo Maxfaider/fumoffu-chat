63e87467eb79932b6293fbcf99db57f526149ac7
Alan M.E <alanmarquez@outlook.com>
2019-01-07 22:31:26 -0500

    created message-friend component

---
+++ Fumoffu-chat-front/src/componets/message-friend/index.jsx
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+
+import Avatar from '../avatar';
+
+import './message-friend.scss';
+
+export default class MessageFriend extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    renderDate() {
+        if(this.props.date)
+            return <div className="message-friend-date">{ this.props.date }</div>
+    }
+
+    buildMessagewithAvatar() {
+        return (
+            <div className="message-friend">
+                <Avatar username={ this.props.username } profileUrl={ this.props.profileUrl }/>
+                <div className="text">
+                    { this.props.text }
+                </div>
+            </div>
+        )
+    }
+
+    buildMessage() {
+        return (
+            <div className="message-friend">
+                <div className="text-only" date={ this.props.date }>
+                    { this.props.text }
+                </div>
+            </div>
+        )
+    }
+    
+    render() {
+        const isProfile = this.props.profileUrl;
+        return (
+          <div>
+            { isProfile ? this.buildMessagewithAvatar() : this.buildMessage() }
+            { this.renderDate() }
+          </div>
+        );
+    }    
+}
